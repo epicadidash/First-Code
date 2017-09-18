bc0036c1643389e745cbff0d9f1f1e2db45ed7dc
epicadidash <dradwaitdash@gmail.com>
2017-09-18 17:39:38 +0530

    I am Adidash3745M

--- star.java
+++ star.java
@@ -2,6 +2,30 @@ import java.util.Scanner;
 
 class Calculator {
     public static void main(String[] args) {
-System.out.println("HEllo world");
+        Scanner X = new Scanner(System.in);
+        // Declare variables
+        int a, b, sum, diff, prod, quo, rem;
+
+        // Get command line argument values
+   System.out.println("Enter a number ");
+   a = X.nextInt();
+   System.out.println("Enter another number ");
+   b = X.nextInt();
+
+        // Find sum, difference, product, quotient and remainder
+        sum = a + b;
+        diff = a - b;
+        prod = a * b;
+        quo = a / b;
+        rem = a % b;
+
+        // print result
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + diff);
+        System.out.println("Product = " + prod);
+        System.out.println("Quotient = " + quo);
+        System.out.println("Remainder = " + rem);
+        X.close();
     }
+    //2020
 }
